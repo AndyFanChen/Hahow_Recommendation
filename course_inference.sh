df58a328ff528ce0e95b96ac2136b9ea79819f97
Yun-Fan Chen <99866172+AndyFanChen@users.noreply.github.com>
2024-02-20 16:01:01 +0800

    Add files via upload

---
+++ course_inference.sh
@@ -0,0 +1,1 @@
+python dropoutNet/torch/inference.py --data-dir data --model_path checkpoint/0_9_model.ckpt --task $1 --output predict/course_$1_predict.csv
---
+++ subgroup_train.sh
@@ -0,0 +1,3 @@
+
+
+python multiLabel/multi.py data
